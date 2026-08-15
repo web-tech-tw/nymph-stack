80c6b62759edb27f8fa88a0fd37f12f3b321645e
不知火 Shiranui <supersonic@livemail.tw>
2026-08-15 10:01:10 +0800

    feat(000-rustic,010-rem): add stack-up condition check and docker build

--- 010-rem/prepare.sh
+++ 010-rem/prepare.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+docker-compose \
+    build --no-cache
+
+if [ "$NYMPH_SCRIPT_NAME" != "stack-up" ]; then
+    echo "Not stack-up, skip..."
+    exit 0
+fi
+
 envsubst \
     <"config.yaml.tmpl" \
     >"config.yaml"
-
-docker-compose \
-    build --no-cache
