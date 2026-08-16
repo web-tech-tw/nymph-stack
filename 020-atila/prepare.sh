6684f2eb1064b8be2d1c5380066ea992697e36dd
不知火 Shiranui <supersonic@livemail.tw>
2026-08-16 17:37:05 +0800

    feat: add atila service with compose, config template, and prepare script

---
+++ 020-atila/prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ "$NYMPH_SCRIPT_NAME" != "stack-up" ]; then
+    echo "Not stack-up, skip..."
+    exit 0
+fi
+
+envsubst \
+    <"config.yaml.tmpl" \
+    >"config.yaml"
