84f1749d585dc4bb0b8b315b504b72cc436af67a
不知火 Shiranui <supersonic@livemail.tw>
2026-08-22 02:00:53 +0800

    chore: add prepare.sh for stack-up environment setup

---
+++ 020-nymph/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ "$NYMPH_SCRIPT_NAME" != "stack-up" ]; then
+    echo "Not stack-up, skip..."
+    exit 0
+fi
+
+envsubst \
+    <"mcp.toml.tmpl" \
+    >"mcp.toml"
+
+chown root:root "mcp.toml"
+chmod 0644 "mcp.toml"
